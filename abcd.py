964df98bc6a8a184294f72a1803e99b2b25f637c
Admin@vi-web-pc <vi-web>
2020-06-17 19:45:29 +0530

    My first commit.

---
+++ abcd.py
@@ -0,0 +1,8 @@
+class Emp:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+rohan = Emp("Rohan", 12999)
+print(rohan.name)
+print(rohan.salary)
---
+++ hangman.py
@@ -0,0 +1,6 @@
+x = 10
+y = 12
+if x%2==0:
+    print("x is 10")
+else:
+    print("its not x"):
